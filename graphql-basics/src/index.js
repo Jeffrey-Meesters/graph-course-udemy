1fa01d21fbb3648eff33dc818c4470d48baf2a67
Jeffrey Meesters <jmeesters@harlemnext.com>
2019-11-10 15:29:45 +0100

    Completed challange

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,5 +1,11 @@
 import currentLocation, { message, greetMe } from "./myModule";
-
+import addNumbers, { subtract } from "./math";
 console.log(message);
 console.log(currentLocation);
 greetMe("you");
+
+const outcome = addNumbers(3, 4);
+console.log(outcome);
+
+const uitslag = subtract(4, 3);
+console.log(uitslag);
