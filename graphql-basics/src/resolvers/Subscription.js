91b022a330c821fe8c5d9499d68317caabf5bad5
Jeffrey Meesters <jmeesters@harlemnext.com>
2019-11-18 21:49:25 +0100

    delete count

--- graphql-basics/src/resolvers/Subscription.js
+++ graphql-basics/src/resolvers/Subscription.js
@@ -1,19 +1,4 @@
 const Subscription = {
-  count: {
-    subscribe(parent, args, { pubsub }, info) {
-      let count = 0;
-
-      setInterval(() => {
-        count++;
-        pubsub.publish("count", {
-          count
-        });
-      }, 1000);
-
-      // the string inside asyncIterator is called: channel name
-      return pubsub.asyncIterator("count");
-    }
-  },
   comment: {
     subscribe(parent, args, { db, pubsub }, info) {
       const { postId } = args;
