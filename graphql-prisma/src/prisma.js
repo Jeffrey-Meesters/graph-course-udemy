456dfce48cbdfc28adbde11d17b5687a41d7fc5c
Jeffrey Meesters <jmeesters@harlemnext.com>
2019-11-23 14:00:32 +0100

    Set up first query and dump it in console

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -2,6 +2,15 @@ import { Prisma } from "prisma-binding";
 
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
-  endpoint: "localhost:4466",
+  endpoint: "http://localhost:4466",
   secret: ""
+});
+
+// prisma.query
+// prisma.mutation
+// prisma.subscription
+// prisma.exists
+
+prisma.query.users(null, "{ id name email }").then(data => {
+  console.log(data);
 });
