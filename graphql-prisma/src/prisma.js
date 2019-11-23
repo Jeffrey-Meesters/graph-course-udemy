855a7b5d90ad71d08c5da65a3ddf40e32c7f5905
Jeffrey Meesters <jmeesters@harlemnext.com>
2019-11-23 14:23:51 +0100

    Complete chellenge create prisma mutation to update a post and chain promises to return all posts

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -11,10 +11,53 @@ const prisma = new Prisma({
 // prisma.subscription
 // prisma.exists
 
-prisma.query.users(null, "{ id name email posts { id title } }").then(data => {
-  console.log(JSON.stringify(data, undefined, 2));
-});
+// prisma.query.users(null, "{ id name email posts { id title } }").then(data => {
+//   console.log(JSON.stringify(data, undefined, 2));
+// });
 
-prisma.query.comments(null, "{ id text author {id name } }").then(data => {
-  console.log(JSON.stringify(data, undefined, 2));
-});
+// prisma.query.comments(null, "{ id text author {id name } }").then(data => {
+//   console.log(JSON.stringify(data, undefined, 2));
+// });
+
+// prisma.mutation
+//   .createPost(
+//     {
+//       data: {
+//         title: "My new post",
+//         body: "Hi yo!",
+//         published: false,
+//         author: {
+//           connect: {
+//             id: "ck3bixyxx00xd0731g5x0pn4c"
+//           }
+//         }
+//       }
+//     },
+//     "{ id title body published }"
+//   )
+//   .then(data => {
+//     // console.log(JSON.stringify(data, undefined, 2));
+//     return prisma.query.users(null, "{ id name email posts { id title } }");
+//   })
+//   .then(data => {
+//     console.log(JSON.stringify(data, undefined, 2));
+//   });
+
+prisma.mutation
+  .updatePost({
+    data: {
+      published: true
+    },
+    where: {
+      id: "ck3bljmci029i0731i658tuk9"
+    }
+  })
+  .then(() => {
+    return prisma.query.posts(null, "{ id title body published }");
+  })
+  .then(data => {
+    console.log(JSON.stringify(data, undefined, 2));
+  })
+  .catch(error => {
+    console.log(error);
+  });
