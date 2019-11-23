813c32f9f4feb11b7df82e8535cab5876b0c6fc7
Jeffrey Meesters <jmeesters@harlemnext.com>
2019-11-23 13:47:13 +0100

    Set up graphql ci to fetch schema from prisma

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -1,7 +1,7 @@
 import { Prisma } from "prisma-binding";
 
 const prisma = new Prisma({
-  typeDefs: "",
+  typeDefs: "src/generated/prisma.graphql",
   endpoint: "localhost:4466",
   secret: ""
 });
