3d9324afd1ad49f2ca245f0ef026d21e885feef7
Jeffrey Meesters <jmeesters@harlemnext.com>
2019-12-01 13:56:45 +0100

    Learn using field resolvers

--- graphql-prisma/src/resolvers/User.js
+++ graphql-prisma/src/resolvers/User.js
@@ -1,5 +1,23 @@
+import getUserId from "../utils/getUserId"
+
 const User = {
   // prisma fetches relational data so no need to create this ourselves
+  // But here we can restrict data responses
+
+  email(parent, args, {
+    request
+  }, info) {
+    const userId = getd(request, false);
+
+    if (userId && userId === parent.id) {
+      return parent.email;
+    }
+
+    return null;
+  }
 };
 
-export { User as default };
+export {
+  User as
+  default
+};
